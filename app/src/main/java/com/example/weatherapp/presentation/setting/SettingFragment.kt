b27979737dc45280ac043315283e5f42afa1aead
Nguyễn Ngọc Toàn <73936452+ngoctoan99@users.noreply.github.com>
2025-01-24 14:05:48 +0700

    fix : set notification

--- app/src/main/java/com/example/weatherapp/presentation/setting/SettingFragment.kt
+++ app/src/main/java/com/example/weatherapp/presentation/setting/SettingFragment.kt
@@ -1,4 +1,5 @@
 package com.example.weatherapp.presentation.setting
+
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.content.ComponentName
@@ -6,23 +7,17 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
 import android.widget.Toast
 import androidx.core.app.NotificationManagerCompat
-import android.provider.Settings
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
-
 import androidx.fragment.app.viewModels
 import com.example.weatherapp.BuildConfig
-import com.example.weatherapp.R
 import com.example.weatherapp.base.BaseFragment
 import com.example.weatherapp.data.local.CachePreferencesHelper
 import com.example.weatherapp.databinding.FragmentSettingBinding
-import com.example.weatherapp.extension.MANDATORY_PERMISSIONS_APP
 import com.example.weatherapp.presentation.weather.WeatherViewModel
 import com.example.weatherapp.presentation.weatherWidget.WeatherWidgetProvider
-
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -56,31 +51,11 @@ class SettingFragment : BaseFragment<FragmentSettingBinding, WeatherViewModel>()
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        updateUISettingNotification()
-    }
-
-    private fun updateUISettingNotification(){
-    if(isNotificationPermissionGranted()){
-        binding.tvSettingNotification.text = getString(R.string.disable_notification)
-    }else {
-        binding.tvSettingNotification.text = getString(R.string.enable_notification)
-    }
-    }
     private fun openNotificationSettings() {
         val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS).apply {
             putExtra(Settings.EXTRA_APP_PACKAGE, BuildConfig.APPLICATION_ID)
         }
         startActivity(intent)
-    }
-
-    private fun isNotificationPermissionGranted(): Boolean {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            requireActivity().checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
-        } else {
-            NotificationManagerCompat.from(requireContext()).areNotificationsEnabled()
-        }
     }
 
     private fun checkWidget() : Boolean {
