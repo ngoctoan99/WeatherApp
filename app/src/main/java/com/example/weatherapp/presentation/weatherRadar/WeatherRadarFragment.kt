2c16ec12381258ac068344e027eb6965bc3c7324
Nguyễn Ngọc Toàn <73936452+ngoctoan99@users.noreply.github.com>
2025-01-24 14:44:48 +0700

    feat : config webview

--- app/src/main/java/com/example/weatherapp/presentation/weatherRadar/WeatherRadarFragment.kt
+++ app/src/main/java/com/example/weatherapp/presentation/weatherRadar/WeatherRadarFragment.kt
@@ -3,6 +3,7 @@ package com.example.weatherapp.presentation.weatherRadar
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebSettings
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.example.weatherapp.base.BaseFragment
@@ -11,6 +12,7 @@ import com.example.weatherapp.databinding.FragmentWeatherRadarBinding
 import com.example.weatherapp.extension.jsonToObjectUsingMoshi
 import com.example.weatherapp.presentation.weather.WeatherViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -28,7 +30,11 @@ class WeatherRadarFragment : BaseFragment<FragmentWeatherRadarBinding, WeatherVi
     @SuppressLint("SetJavaScriptEnabled")
     private fun initView() {
         binding.apply {
-            webView.settings.javaScriptEnabled = true
+            webView.settings.apply {
+                javaScriptEnabled = true
+                domStorageEnabled = true  // Cho phép lưu trữ dữ liệu DOM
+                cacheMode = WebSettings.LOAD_DEFAULT
+            }
             if(cachePreferencesHelper.dataWeather.isNotEmpty()){
                 val data = jsonToObjectUsingMoshi(cachePreferencesHelper.dataWeather)
                 webView.loadUrl("https://embed.windy.com/embed2.html?lat=${data?.latitude}&lon=${data?.longitude}&zoom=15&overlay=radar&menu=&message=&marker=&calendar=&pressure=&type=map&location=coordinates&detail=&detailLat=${data?.latitude}&detailLon=${data?.longitude}&metricWind=default&metricTemp=default&radarRange=-1")
@@ -37,5 +43,15 @@ class WeatherRadarFragment : BaseFragment<FragmentWeatherRadarBinding, WeatherVi
                 webView.loadUrl("https://embed.windy.com/embed2.html?lat=10.762622&lon=106.660172&zoom=15&overlay=radar&menu=&message=&marker=&calendar=&pressure=&type=map&location=coordinates&detail=&detailLat=10.762622&detailLon=106.660172&metricWind=default&metricTemp=default&radarRange=-1")
             }
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.apply {
+            webView.clearCache(true)
+            webView.clearHistory()
+            webView.clearFormData()
+        }
+        Timber.i("TTT::onStop")
     }
 }
